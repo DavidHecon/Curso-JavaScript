8e9c64dc41267a07b71835eff7c3f2efa22734c5
David Hernández Contreras <100537779+DavidHecon@users.noreply.github.com>
2025-06-09 21:07:02 -0600

    Add files via upload

---
+++ hooks/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ListaCompras from "./ListaCompras";
+
+function App() {
+  return (
+    <div>
+      <h1>Mi Aplicación de Compras</h1>
+      <ListaCompras />
+    </div>
+  );
+}
+
+export default App;
